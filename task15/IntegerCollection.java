12d11e71f480689c6d5abae3d7cb3f4874696029
xaazias <flamesuspect@gmail.com>
2020-10-28 20:24:41 +0300

    Updated Task15

--- task15/IntegerCollection.java
+++ task15/IntegerCollection.java
@@ -1,33 +1,65 @@
 package ru.mirea.java.task15;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class IntegerCollection {
 
-    private final ArrayList<Integer> collection = new ArrayList<>();
+    private int[] collection = new int[] {};
 
     public void push(int item) {
-        this.collection.add(item);
+
+        int[] buffer = new int[collection.length + 1];
+
+        System.arraycopy(collection, 0, buffer, 0, collection.length);
+
+        buffer[collection.length] = item;
+
+        this.collection = buffer;
     }
 
     public void pop(int index) {
-        this.collection.remove(index);
+
+        if (collection == null || index < 0 || index >= collection.length)
+            return;
+
+        this.collection = IntStream.range(0, collection.length)
+            .filter(i -> i != index)
+            .map(i -> collection[i])
+            .toArray();
     }
 
     public int findByValue(int value) {
-        return this.collection.indexOf(value);
+
+        for (int i = 0; i < collection.length; i++)
+            if (collection[i] == value) return i;
+
+        return -1;
     }
 
     public int findByIndex(int index) {
-        return this.collection.get(index);
+
+        return this.collection[index];
     }
 
     public int getMax() {
-        return Collections.max(collection);
+
+        int max = collection[0];
+
+        for (int i : collection) {
+            if (i > max) max = i;
+        }
+        return max;
     }
 
     public int getMin() {
-        return Collections.min(collection);
+
+        int min = collection[0];
+
+        for (int i : collection) {
+            if (i < min) min = i;
+        }
+        return min;
     }
 
     public double getAvg() {
@@ -37,13 +69,11 @@ public class IntegerCollection {
         for (Integer integer : collection) {
             sum += integer;
         }
-        return (double) Math.round(sum / collection.size() * 100) / 100;
+        return (double) Math.round(sum / collection.length * 100) / 100;
     }
 
     @Override
     public String toString() {
-        return "IntegerCollection{"
-                + collection +
-                '}';
+        return "IntegerCollection : " + Arrays.toString(collection);
     }
 }
